04567995899dcb400f23a1e719734bcf3902cdb4
tsutsui_kazunari <112367729+bossun11@users.noreply.github.com>
2023-05-02 17:21:53 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //li作成
+  const li = document.createElement("li");
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // pタグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  //divの子要素に各要素を設定
+  li.appendChild(div);
+  div.appendChild(p);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
